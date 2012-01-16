510990ad8d15d43ddfd8a7e20eea555f19d5d6da
Jeremy Wood <farachan@gmail.com>
2012-01-16 12:35:05 -0500

    Added PersistingGroup data class.

---
+++ src/main/java/com/onarandombox/multiverseinventories/group/SimplePersistingGroup.java
@@ -0,0 +1,33 @@
+package com.onarandombox.multiverseinventories.group;
+
+import com.onarandombox.multiverseinventories.share.Sharable;
+
+/**
+ * Simple implementation of PersistingGroup
+ */
+public class SimplePersistingGroup implements PersistingGroup {
+
+    private Sharable sharable;
+    private WorldGroup group;
+
+    public SimplePersistingGroup(Sharable sharable, WorldGroup group) {
+        this.sharable = sharable;
+        this.group = group;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Sharable getSharable() {
+        return this.sharable;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public WorldGroup getGroup() {
+        return this.group;
+    }
+}
