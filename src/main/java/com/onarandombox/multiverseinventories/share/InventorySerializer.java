9fbdb74724f166016f1ac1178028c90bee283e9c
Jeremy Wood <farachan@gmail.com>
2017-06-23 13:48:34 -0400

    Significant simplification of inventory (de)serialization.

--- src/main/java/com/onarandombox/multiverseinventories/share/InventorySerializer.java
+++ src/main/java/com/onarandombox/multiverseinventories/share/InventorySerializer.java
@@ -1,7 +1,11 @@
 package com.onarandombox.multiverseinventories.share;
 
-import com.onarandombox.multiverseinventories.DataStrings;
+import com.onarandombox.multiverseinventories.util.MinecraftTools;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link SharableSerializer} usable with ItemStack[] which converts the ItemStack[] to the string format
@@ -17,12 +21,37 @@ public final class InventorySerializer implements SharableSerializer<ItemStack[]
 
     @Override
     public ItemStack[] deserialize(Object obj) {
-        return DataStrings.parseInventory(obj, this.inventorySize);
+        return unmapSlots(obj);
     }
 
     @Override
     public Object serialize(ItemStack[] itemStacks) {
-        //return DataStrings.valueOf(itemStacks);
-        return DataStrings.asJsonObject(itemStacks, inventorySize);
+        return mapSlots(itemStacks);
+    }
+
+    private Map<String, ItemStack> mapSlots(ItemStack[] itemStacks) {
+        Map<String, ItemStack> result = new HashMap<>(itemStacks.length);
+        for (int i = 0; i < itemStacks.length; i++) {
+            if (itemStacks[i] != null && itemStacks[i].getType() != Material.AIR) {
+                result.put(Integer.toString(i), itemStacks[i]);
+            }
+        }
+        return result;
+    }
+
+    private ItemStack[] unmapSlots(Object obj) {
+        ItemStack[] result = new ItemStack[inventorySize];
+        if (obj instanceof Map) {
+            Map<?, ?> invMap = (Map) obj;
+            for (int i = 0; i < result.length; i++) {
+                Object value = invMap.get(Integer.toString(i));
+                if (value != null && value instanceof ItemStack) {
+                    result[i] = (ItemStack) value;
+                } else {
+                    result[i] = new ItemStack(Material.AIR);
+                }
+            }
+        }
+        return MinecraftTools.fillWithAir(result);
     }
 }
