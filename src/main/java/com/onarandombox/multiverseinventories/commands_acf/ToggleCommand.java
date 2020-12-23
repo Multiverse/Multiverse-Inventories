7b5fd510cfc597cef49143f83c3accff6d8218fc
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2020-12-23 15:15:02 +0800

    Improve toggle command with context and condtion.

--- src/main/java/com/onarandombox/multiverseinventories/commands_acf/ToggleCommand.java
+++ src/main/java/com/onarandombox/multiverseinventories/commands_acf/ToggleCommand.java
@@ -2,14 +2,13 @@ package com.onarandombox.multiverseinventories.commands_acf;
 
 import com.onarandombox.acf.annotation.CommandAlias;
 import com.onarandombox.acf.annotation.CommandCompletion;
+import com.onarandombox.acf.annotation.Conditions;
 import com.onarandombox.acf.annotation.Description;
 import com.onarandombox.acf.annotation.Subcommand;
 import com.onarandombox.acf.annotation.Syntax;
 import com.onarandombox.multiverseinventories.MultiverseInventories;
 import com.onarandombox.multiverseinventories.locale.Message;
 import com.onarandombox.multiverseinventories.share.Sharable;
-import com.onarandombox.multiverseinventories.share.Sharables;
-import com.onarandombox.multiverseinventories.share.Shares;
 import com.onarandombox.multiverseinventories.util.Perm;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
@@ -24,37 +23,21 @@ public class ToggleCommand extends InventoriesCommand {
 
     @Subcommand("toggle")
     @Syntax("<share>")
-    @CommandCompletion("")
+    @CommandCompletion("@optionalSharables")
     @Description("Toggles the usage of optional sharable.")
     public void onToggleCommand(@NotNull CommandSender sender,
-                                @NotNull String targetSharable) {
+                                @NotNull @Conditions("optional") Sharable<?> targetSharable) {
 
-        //TODO: Move this to command conditions.
-        Shares shares = Sharables.lookup(targetSharable.toLowerCase());
-        if (shares == null) {
-            this.messager.normal(Message.ERROR_NO_SHARES_SPECIFIED, sender);
-            return;
+        //TODO: on|off toggle instead?
+        if (this.plugin.getMVIConfig().getOptionalShares().contains(targetSharable)) {
+            this.plugin.getMVIConfig().getOptionalShares().remove(targetSharable);
+            this.messager.normal(Message.NOW_NOT_USING_OPTIONAL, sender, targetSharable.getNames()[0]);
+        }
+        else {
+            this.plugin.getMVIConfig().getOptionalShares().add(targetSharable);
+            this.messager.normal(Message.NOW_USING_OPTIONAL, sender, targetSharable.getNames()[0]);
         }
 
-        boolean foundOpt = false;
-        for (Sharable<?> sharable : shares) {
-            if (sharable.isOptional()) {
-                foundOpt = true;
-                if (this.plugin.getMVIConfig().getOptionalShares().contains(sharable)) {
-                    this.plugin.getMVIConfig().getOptionalShares().remove(sharable);
-                    this.messager.normal(Message.NOW_NOT_USING_OPTIONAL, sender, sharable.getNames()[0]);
-                } else {
-                    this.plugin.getMVIConfig().getOptionalShares().add(sharable);
-                    this.messager.normal(Message.NOW_USING_OPTIONAL, sender, sharable.getNames()[0]);
-                }
-                break;
-            }
-        }
-
-        if (foundOpt) {
-            this.plugin.getMVIConfig().save();
-        } else {
-            this.messager.normal(Message.NO_OPTIONAL_SHARES, sender, targetSharable);
-        }
+        this.plugin.getMVIConfig().save();
     }
 }
--- src/main/java/com/onarandombox/multiverseinventories/util/CommandTools.java
+++ src/main/java/com/onarandombox/multiverseinventories/util/CommandTools.java
@@ -1,27 +1,103 @@
 package com.onarandombox.multiverseinventories.util;
 
 import com.onarandombox.MultiverseCore.commandTools.MVCommandManager;
+import com.onarandombox.acf.BukkitCommandCompletionContext;
+import com.onarandombox.acf.BukkitCommandExecutionContext;
+import com.onarandombox.acf.BukkitCommandIssuer;
+import com.onarandombox.acf.ConditionContext;
+import com.onarandombox.acf.ConditionFailedException;
+import com.onarandombox.acf.InvalidCommandArgument;
 import com.onarandombox.multiverseinventories.MultiverseInventories;
 import com.onarandombox.multiverseinventories.commands_acf.GroupCommand;
 import com.onarandombox.multiverseinventories.commands_acf.ToggleCommand;
+import com.onarandombox.multiverseinventories.locale.Message;
+import com.onarandombox.multiverseinventories.locale.Messager;
+import com.onarandombox.multiverseinventories.share.Sharable;
+import com.onarandombox.multiverseinventories.share.Sharables;
+import com.onarandombox.multiverseinventories.share.Shares;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class CommandTools {
 
     private final MultiverseInventories plugin;
     private final MVCommandManager manager;
+    private final Messager messager;
 
     public CommandTools(MultiverseInventories plugin) {
         this.plugin = plugin;
         this.manager = plugin.getCore().getMVCommandManager();
+        this.messager = plugin.getMessager();
 
         // Completions
+        this.manager.getCommandCompletions().registerAsyncCompletion("sharables", this::suggestSharables);
+        this.manager.getCommandCompletions().registerAsyncCompletion("optionalSharables", this::suggestOptionalSharables);
 
         // Contexts
+        this.manager.getCommandContexts().registerContext(Sharable.class, this::deriveSharable);
+        this.manager.getCommandContexts().registerContext(Shares.class, this::deriveShares);
 
         // Conditions
+        this.manager.getCommandConditions().addCondition(Sharable.class, "optional", this::checkIsSharableOptional);
 
         //Commands
         this.manager.registerCommand(new GroupCommand(this.plugin));
         this.manager.registerCommand(new ToggleCommand(this.plugin));
+    }
+
+    @NotNull
+    private Collection<String> suggestSharables(@NotNull BukkitCommandCompletionContext context) {
+        return Sharables.all().stream()
+                .unordered()
+                .map(share -> share.getNames()[0])
+                .collect(Collectors.toList());
+    }
+
+    @NotNull
+    private Collection<String> suggestOptionalSharables(@NotNull BukkitCommandCompletionContext context) {
+        return Sharables.all().stream()
+                .unordered()
+                .filter(Sharable::isOptional)
+                .map(share -> share.getNames()[0])
+                .collect(Collectors.toList());
+    }
+
+    @NotNull
+    private Sharable<?> deriveSharable(BukkitCommandExecutionContext context) {
+        String shareName = context.popFirstArg();
+        Sharable<?> targetSharable = Sharables.all().stream()
+                .unordered()
+                .filter(sharable -> sharable.getNames()[0].equals(shareName))
+                .findFirst()
+                .orElse(null);
+
+        if (targetSharable == null) {
+            this.messager.normal(Message.ERROR_NO_SHARES_SPECIFIED, context.getSender());
+            throw new InvalidCommandArgument();
+        }
+
+        return targetSharable;
+    }
+
+    @NotNull
+    private Shares deriveShares(BukkitCommandExecutionContext context) {
+        Shares shares = Sharables.lookup(context.popFirstArg());
+        if (shares == null) {
+            this.messager.normal(Message.ERROR_NO_SHARES_SPECIFIED, context.getSender());
+            throw new InvalidCommandArgument();
+        }
+        return shares;
+    }
+
+    private void checkIsSharableOptional(@NotNull ConditionContext<BukkitCommandIssuer> context,
+                                         @NotNull BukkitCommandExecutionContext executionContext,
+                                         @NotNull Sharable<?> sharable) {
+
+        if (!sharable.isOptional()) {
+            this.messager.normal(Message.NO_OPTIONAL_SHARES, executionContext.getSender(), sharable);
+            throw new ConditionFailedException();
+        }
     }
 }
