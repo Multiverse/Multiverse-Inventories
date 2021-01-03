191c6396cedfd955c914d9e0b0fdadb9459a657e
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2021-01-03 16:52:31 +0800

    list command use ListDisplay.

--- src/main/java/com/onarandombox/multiverseinventories/commands/ListCommand.java
+++ src/main/java/com/onarandombox/multiverseinventories/commands/ListCommand.java
@@ -1,5 +1,8 @@
 package com.onarandombox.multiverseinventories.commands;
 
+import com.onarandombox.MultiverseCore.commandTools.display.ContentCreator;
+import com.onarandombox.MultiverseCore.commandTools.display.ContentFilter;
+import com.onarandombox.MultiverseCore.commandTools.display.inline.ListDisplay;
 import com.onarandombox.acf.annotation.CommandAlias;
 import com.onarandombox.acf.annotation.Description;
 import com.onarandombox.acf.annotation.Subcommand;
@@ -7,10 +10,13 @@ import com.onarandombox.multiverseinventories.MultiverseInventories;
 import com.onarandombox.multiverseinventories.WorldGroup;
 import com.onarandombox.multiverseinventories.locale.Message;
 import com.onarandombox.multiverseinventories.util.Perm;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @CommandAlias("mvinv")
 public class ListCommand extends InventoriesCommand {
@@ -20,24 +26,26 @@ public class ListCommand extends InventoriesCommand {
         this.addPermission(Perm.COMMAND_LIST);
     }
 
-    @Subcommand("list")
+    @Subcommand("list [filter]")
     @Description("World and Group Information.")
-    public void onListCommand(@NotNull CommandSender sender) {
-        this.messager.normal(Message.LIST_GROUPS, sender, generateGroupString());
+    public void onListCommand(@NotNull CommandSender sender,
+                              @NotNull ContentFilter filter) {
+
+        String prefix = this.messager.getMessage(Message.LIST_GROUPS_PREFIX);
+
+        new ListDisplay().withSender(sender)
+                .withHeader(this.messager.getMessage(Message.LIST_GROUPS_HEADER))
+                .withCreator(generateGroupContent())
+                .withPrefix(prefix)
+                .withFilter(filter)
+                .withEmptyMessage(String.format("%sN/A", prefix))
+                .build()
+                .runTaskAsynchronously(this.plugin);
     }
 
-    private String generateGroupString() {
-        Collection<WorldGroup> groups = this.plugin.getGroupManager().getGroups();
-        if (groups.isEmpty()) {
-            return "N/A";
-        }
-        StringBuilder builder = new StringBuilder();
-        for (WorldGroup group : groups) {
-            if (builder.length() > 0) {
-                builder.append(", ");
-            }
-            builder.append(group.getName());
-        }
-        return builder.toString();
+    private ContentCreator<List<String>> generateGroupContent() {
+        return () -> this.plugin.getGroupManager().getGroups().stream()
+                .map(WorldGroup::getName)
+                .collect(Collectors.toList());
     }
 }
