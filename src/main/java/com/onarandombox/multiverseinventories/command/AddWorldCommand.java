9be1dff9d694a634621b4339c7fae3fddd825caf
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2021-03-04 17:58:03 +0800

    Add ability to ignore if world exist when adding/removing world to/from group.

--- src/main/java/com/onarandombox/multiverseinventories/command/AddWorldCommand.java
+++ src/main/java/com/onarandombox/multiverseinventories/command/AddWorldCommand.java
@@ -1,11 +1,13 @@
 package com.onarandombox.multiverseinventories.command;
 
+import com.onarandombox.commandhandler.CommandHandler;
 import com.onarandombox.multiverseinventories.MultiverseInventories;
 import com.onarandombox.multiverseinventories.WorldGroup;
 import com.onarandombox.multiverseinventories.locale.Message;
 import com.onarandombox.multiverseinventories.util.Perm;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
 import java.util.List;
@@ -20,8 +22,8 @@ public class AddWorldCommand extends InventoriesCommand {
     public AddWorldCommand(MultiverseInventories plugin) {
         super(plugin);
         this.setName("Adds a World to a World Group.");
-        this.setCommandUsage("/mvinv addworld {WORLD} {GROUP}");
-        this.setArgRange(2, 2);
+        this.setCommandUsage("/mvinv addworld {WORLD} {GROUP} [--ignore-exist]");
+        this.setArgRange(2, 3);
         this.addKey("mvinv addworld");
         this.addKey("mvinv addw");
         this.addKey("mvinvaw");
@@ -32,8 +34,8 @@ public class AddWorldCommand extends InventoriesCommand {
 
     @Override
     public void runCommand(CommandSender sender, List<String> args) {
-        World world = Bukkit.getWorld(args.get(0));
-        if (world == null) {
+        String worldName = args.get(0);
+        if (!CommandHandler.hasFlag("--ignore-exist", args) && Bukkit.getWorld(worldName) == null) {
             this.messager.normal(Message.ERROR_NO_WORLD, sender, args.get(0));
             return;
         }
@@ -42,15 +44,15 @@ public class AddWorldCommand extends InventoriesCommand {
             this.messager.normal(Message.ERROR_NO_GROUP, sender, args.get(1));
             return;
         }
-        if (worldGroup.containsWorld(world.getName())) {
-            this.messager.normal(Message.WORLD_ALREADY_EXISTS, sender, world.getName(),
+        if (worldGroup.containsWorld(worldName)) {
+            this.messager.normal(Message.WORLD_ALREADY_EXISTS, sender, worldName,
                     worldGroup.getName());
             return;
         }
-        worldGroup.addWorld(world);
+        worldGroup.addWorld(worldName);
         this.plugin.getGroupManager().updateGroup(worldGroup);
         this.plugin.getMVIConfig().save();
-        this.messager.normal(Message.WORLD_ADDED, sender, world.getName(),
+        this.messager.normal(Message.WORLD_ADDED, sender, worldName,
                 worldGroup.getName());
     }
 }
