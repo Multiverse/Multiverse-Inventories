c096041f0f911fad8026530222ef62806a703130
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2025-01-19 14:19:48 +0800

    Implement helper class SingleShareWriter

---
+++ src/main/java/org/mvplugins/multiverse/inventories/SingleShareWriter.java
@@ -0,0 +1,43 @@
+package org.mvplugins.multiverse.inventories;
+
+import org.bukkit.entity.Player;
+import org.mvplugins.multiverse.inventories.share.Sharable;
+
+/**
+ * Write a single share to the relevant world and group profiles.
+ *
+ * @param <T>   The sharable type.
+ */
+public class SingleShareWriter<T> {
+
+    public static <T> SingleShareWriter<T> of(MultiverseInventories inventories, Player player, Sharable<T> sharable) {
+        return new SingleShareWriter<T>(inventories, player, sharable);
+    }
+
+    private final MultiverseInventories inventories;
+    private final Player player;
+    private final Sharable<T> sharable;
+
+    private SingleShareWriter(MultiverseInventories inventories, Player player, Sharable<T> sharable) {
+        this.inventories = inventories;
+        this.player = player;
+        this.sharable = sharable;
+    }
+
+    public void write(T value) {
+        if (sharable.isOptional() && !this.inventories.getMVIConfig().getOptionalShares().contains(sharable)) {
+            return;
+        }
+
+        String worldName = this.player.getWorld().getName();
+        this.inventories.getWorldProfileContainerStore()
+                .getContainer(worldName)
+                .getPlayerData(this.player)
+                .set(this.sharable, value);
+
+        this.inventories.getGroupManager().getGroupsForWorld(worldName).forEach(worldGroup -> {
+            worldGroup.getGroupProfileContainer().getPlayerData(this.player)
+                    .set(this.sharable, value);
+        });
+    }
+}
