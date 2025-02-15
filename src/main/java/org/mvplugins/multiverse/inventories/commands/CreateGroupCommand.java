af16d5973869ba193f096ad3ca9bb4c4cf80225e
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2025-02-15 15:58:39 +0800

    Add worlds and shares params to create command

--- src/main/java/org/mvplugins/multiverse/inventories/commands/CreateGroupCommand.java
+++ src/main/java/org/mvplugins/multiverse/inventories/commands/CreateGroupCommand.java
@@ -3,17 +3,23 @@ package org.mvplugins.multiverse.inventories.commands;
 import org.jvnet.hk2.annotations.Service;
 import org.mvplugins.multiverse.core.commandtools.MVCommandIssuer;
 import org.mvplugins.multiverse.core.commandtools.MVCommandManager;
+import org.mvplugins.multiverse.core.world.MultiverseWorld;
 import org.mvplugins.multiverse.external.acf.commands.annotation.CommandAlias;
+import org.mvplugins.multiverse.external.acf.commands.annotation.CommandCompletion;
 import org.mvplugins.multiverse.external.acf.commands.annotation.CommandPermission;
 import org.mvplugins.multiverse.external.acf.commands.annotation.Conditions;
 import org.mvplugins.multiverse.external.acf.commands.annotation.Description;
+import org.mvplugins.multiverse.external.acf.commands.annotation.Optional;
 import org.mvplugins.multiverse.external.acf.commands.annotation.Subcommand;
 import org.mvplugins.multiverse.external.acf.commands.annotation.Syntax;
 import org.mvplugins.multiverse.external.jakarta.inject.Inject;
 import org.mvplugins.multiverse.external.jetbrains.annotations.NotNull;
 import org.mvplugins.multiverse.inventories.profile.group.WorldGroup;
 import org.mvplugins.multiverse.inventories.profile.group.WorldGroupManager;
+import org.mvplugins.multiverse.inventories.share.Shares;
 import org.mvplugins.multiverse.inventories.util.MVInvi18n;
+
+import java.util.Arrays;
 
 import static org.mvplugins.multiverse.core.locale.message.MessageReplacement.replace;
 
@@ -30,7 +36,8 @@ final class CreateGroupCommand extends InventoriesCommand {
 
     @Subcommand("creategroup")
     @CommandPermission("multiverse.inventories.creategroup")
-    @Syntax("<name>")
+    @CommandCompletion("@empty @mvworlds:multiple,scope=both, @shares")
+    @Syntax("<name> [share[,extra]] [world[,extra]]")
     @Description("Creates a new empty World Group with no worlds and no shares.")
     void onCreateGroupCommand(
             MVCommandIssuer issuer,
@@ -38,10 +45,26 @@ final class CreateGroupCommand extends InventoriesCommand {
             @Conditions("newWorldGroupName")
             @Syntax("<name>")
             @Description("New group name to create.")
-            @NotNull String groupName
+            @NotNull String groupName,
+
+            @Optional
+            @Syntax("[world[,extra]]")
+            MultiverseWorld[] worlds,
+
+            @Optional
+            @Syntax("[share,[extra]]")
+            Shares shares
     ) {
         WorldGroup worldGroup = worldGroupManager.newEmptyGroup(groupName);
+        if (worlds != null) {
+            worldGroup.getWorlds().addAll(Arrays.stream(worlds).map(MultiverseWorld::getName).toList());
+        }
+        if (shares != null) {
+            worldGroup.getShares().mergeShares(shares);
+        }
         worldGroupManager.updateGroup(worldGroup);
         issuer.sendInfo(MVInvi18n.GROUP_CREATIONCOMPLETE, replace("{group}").with(groupName));
+        issuer.sendInfo(MVInvi18n.INFO_GROUP_INFO, replace("{worlds}").with(worldGroup.getWorlds()));
+        issuer.sendInfo(MVInvi18n.INFO_GROUP_INFOSHARES, replace("{shares}").with(worldGroup.getShares()));
     }
 }
