1eeaa06371c96eeb4bffda7bfedcc06454b3ceec
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2025-03-01 22:59:33 +0800

    Add basic config test

--- src/test/java/org/mvplugins/multiverse/inventories/config/ConfigTest.kt
+++ src/test/java/org/mvplugins/multiverse/inventories/config/ConfigTest.kt
@@ -1,4 +1,32 @@
 package org.mvplugins.multiverse.inventories.config
 
-class ConfigTest {
+import org.mvplugins.multiverse.inventories.TestWithMockBukkit
+import java.io.File
+import java.nio.file.Path
+import kotlin.io.path.absolutePathString
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class ConfigTest : TestWithMockBukkit() {
+
+    private lateinit var config : InventoriesConfig
+    private lateinit var configFile : File
+
+    @BeforeTest
+    fun setUp() {
+        configFile = File(Path.of(multiverseInventories.dataFolder.absolutePath, "config.yml").absolutePathString())
+        if (configFile.exists()) configFile.delete()
+
+        config = serviceLocator.getActiveService(InventoriesConfig::class.java).takeIf { it != null } ?: run {
+            throw IllegalStateException("InventoriesConfig is not available as a service") }
+
+        assertTrue(config.load().isSuccess)
+        assertTrue(config.save().isSuccess)
+    }
+
+    @Test
+    fun `Config is fresh`() {
+        assertConfigEquals("/config/fresh_config.yml", "config.yml")
+    }
 }
