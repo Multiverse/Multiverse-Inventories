3ee9564103a9667fc5326c9b54aece17f805926b
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2025-03-06 22:16:21 +0800

    Add cache stats to debug dumps info

--- src/main/java/org/mvplugins/multiverse/inventories/MVEventsListener.java
+++ src/main/java/org/mvplugins/multiverse/inventories/MVEventsListener.java
@@ -10,6 +10,7 @@ import org.mvplugins.multiverse.core.event.MVDebugModeEvent;
 import org.mvplugins.multiverse.core.event.MVDumpsDebugInfoEvent;
 import org.mvplugins.multiverse.external.jakarta.inject.Inject;
 import org.mvplugins.multiverse.inventories.config.InventoriesConfig;
+import org.mvplugins.multiverse.inventories.profile.ProfileDataSource;
 import org.mvplugins.multiverse.inventories.profile.group.WorldGroup;
 import org.mvplugins.multiverse.inventories.profile.group.WorldGroupManager;
 
@@ -21,12 +22,18 @@ final class MVEventsListener implements Listener {
     private final MultiverseInventories inventories;
     private final InventoriesConfig config;
     private final WorldGroupManager worldGroupManager;
+    private final ProfileDataSource profileDataSource;
 
     @Inject
-    MVEventsListener(@NotNull MultiverseInventories inventories, @NotNull InventoriesConfig config, @NotNull WorldGroupManager worldGroupManager) {
+    MVEventsListener(
+            @NotNull MultiverseInventories inventories,
+            @NotNull InventoriesConfig config,
+            @NotNull WorldGroupManager worldGroupManager,
+            @NotNull ProfileDataSource profileDataSource) {
         this.inventories = inventories;
         this.config = config;
         this.worldGroupManager = worldGroupManager;
+        this.profileDataSource = profileDataSource;
     }
 
     /**
@@ -41,6 +48,24 @@ final class MVEventsListener implements Listener {
         File groupsFile = new File(this.inventories.getDataFolder(), "groups.yml");
         event.putDetailedDebugInfo("multiverse-inventories/config.yml", configFile);
         event.putDetailedDebugInfo("multiverse-inventories/groups.yml", groupsFile);
+        event.putDetailedDebugInfo("multiverse-inventories/cachestats.md", generateCacheStatsContent());
+    }
+
+    private String generateCacheStatsContent() {
+        var builder = new StringBuilder();
+        profileDataSource.getCacheStats().forEach((cacheName, stats) -> {
+            builder.append("# ").append(cacheName).append("\n")
+                    .append("- hits count: ").append(stats.hitCount()).append("\n")
+                    .append("- misses count: ").append(stats.missCount()).append("\n")
+                    .append("- loads count: ").append(stats.loadCount()).append("\n")
+                    .append("- misses count: ").append(stats.missCount()).append("\n")
+                    .append("- evictions: ").append(stats.evictionCount()).append("\n")
+                    .append("- hit rate: ").append(stats.hitRate() * 100).append("%\n")
+                    .append("- miss rate: ").append(stats.missRate() * 100).append("%\n")
+                    .append("- avg load penalty: ").append(stats.averageLoadPenalty() / 1000000).append("ms\n")
+                    .append("\n");
+        });
+        return builder.toString();
     }
 
     /**
