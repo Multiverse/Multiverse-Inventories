7c4ea39a9d0726fcbadacf65cb28374f07acae2e
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2025-04-09 09:56:34 +0800

    Fix command injection test

--- src/test/java/org/mvplugins/multiverse/inventories/InjectionTest.kt
+++ src/test/java/org/mvplugins/multiverse/inventories/InjectionTest.kt
@@ -15,7 +15,7 @@ class InjectionTest : TestWithMockBukkit() {
 
     @Test
     fun `InventoriesCommand are available as a service`() {
-        assertEquals(19, serviceLocator.getAllActiveServices(InventoriesCommand::class.java).size)
+        assertEquals(24, serviceLocator.getAllActiveServices(InventoriesCommand::class.java).size)
     }
 
     @Test
